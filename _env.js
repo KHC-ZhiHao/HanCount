ceb1f631aa0717458f4562fb5332fb70317f114b
KHC-ZhiHao <4a1h0102@stust.edu.tw>
2019-03-30 22:29:20 +0800

    first commit

---
+++ _env.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ip: '靜態網頁ip，設定為auto則交由系統設定',
+    startTime: '2019/01/01',
+    apiKey: 'gcp speech key',
+    youtubeUrl: 'https://www.youtube.com/watch?v=wUPPkSANpyo'
+}
---
+++ index.js
@@ -0,0 +1,99 @@
+// ==========================
+//
+// system
+//
+
+const fs = require('fs')
+const env = require('./env')
+const reg = /韓國|韓總|國瑜|國語|韓市長|高雄市長|挺韓|寒流|韓流|韓|函|涵/g
+
+// ==========================
+//
+// server
+//
+
+const ip = require('ip').address()
+const http = require('http')
+const express = require('express')
+const app = express()
+const server = http.createServer(app)
+const io = require('socket.io')(server)
+
+server.listen(80, env.ip === 'auto' ? ip : env.ip)
+
+app.get('/', ( request, response )=>{
+    let html = fs.readFileSync('./index.html', 'utf8')
+    html = html.replace('--ip--', ip).replace('--keyword--', reg).replace('--begin--', env.startTime)
+    response.write(html)
+    response.end()
+})
+
+io.on('connection', (socket)=>{
+    socket.emit('update', { text: 'wait...', count })
+})
+
+console.log(`< http://${ip} >`)
+
+// ==========================
+//
+// main
+//
+
+const url = env.youtubeUrl
+const apiKey = env.apiKey
+const ytdl = require('ytdl-core')
+const request = require('request')
+const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg')
+const ffmpeg = require('fluent-ffmpeg')
+const ytdlOption = {
+    liveBuffer: 10000
+}
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path)
+
+let recoreFile = './files/minute.json'
+let recore = require(recoreFile)
+let count = recore.count || 0
+let oldBase = null
+
+function action() {
+    let fileName = `./files/${Date.now()}.wav`
+    let stream = ytdl(url, ytdlOption)
+    new ffmpeg(stream).duration(10).audioChannels(1).audioFrequency(8000).format('wav').save(fileName).on('end', () => {
+        stream.end()
+        let base = fs.readFileSync(fileName).toString('base64')
+        if (oldBase === base) return
+        oldBase = base
+        request({
+            uri: 'https://speech.googleapis.com/v1/speech:recognize?key=' + apiKey,
+            method: 'POST',
+            json: {
+                audio: {
+                    content: base
+                },
+                config: {
+                    encoding: 'LINEAR16',
+                    sampleRateHertz: 8000,
+                    languageCode: 'zh-TW',
+                },
+            }
+        }, (error, response, body) => {
+            try{
+                let text = body.results[0].alternatives[0].transcript
+                let match = text.match(reg)
+                if (match) {
+                    count += match.length
+                }
+                console.log('語音 : ', text)
+                console.log('計數 : ', count)
+                io.emit('update', { text, count })
+                fs.writeFileSync(recoreFile, JSON.stringify({ text, count }))
+            } catch(e) {
+                console.log(e.message)
+            }
+            fs.unlinkSync(fileName)
+        })
+    })
+}
+
+setInterval(action, 10000)
