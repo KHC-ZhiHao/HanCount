aa45a7dbc05799bf1ef6e2b8a9c06adc3ede6aad
Ray1422 <30434835+ray1422@users.noreply.github.com>
2019-04-13 17:52:27 +0800

    Update _env.js

--- _env.js
+++ _env.js
@@ -2,8 +2,8 @@ module.exports = {
     ip: '靜態網頁ip，設定為"auto"則交由系統設定',
     publicIp: '設置對外ip，如果沒有可以設定為null',
     startTime: '2020/01/11',
-    appKey: '85fe168a0ab344509e1fce7a031b9f86',
-    appSecret: 'a153198a7cf14eda957eab3bd5daf364',
+    appKey: 'KEY',
+    appSecret: 'SECRET',
     api_base_url: 'https://tw.olami.ai/cloudservice/api',
     keywords: '韓國|國瑜|國語|寒|韓|函|涵',
     youtubeUrl: 'https://www.youtube.com/watch?v=wUPPkSANpyo'
